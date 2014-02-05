37d742108000810915fac0215c81644959ab5168
fnbajwa <farhan.bajwa@hotmail.com>
2014-02-05 12:33:53 +0500

    Test Commit

--- src/main/java/web/controller/BoardController.java
+++ src/main/java/web/controller/BoardController.java
@@ -69,7 +69,6 @@ public class BoardController {
        //UserWrapper userWrapper = new UserWrapper();
        //userWrapper.setUserList(usersList);
        //model.put("uWrapper", userWrapper);
-
        model.put("viewAssignForm", true);
        if(ValidationUtility.isExists(userWrapper.getUserList()))
            for(UserWrapper wr : userWrapper.getUserList()){
