9332d18ff9c498113b1eaefbd71b8a602b7eeb83
fnbajwa <farhan.bajwa@hotmail.com>
2014-04-30 18:51:09 +0500

    Updated Reports DAO and JRXML for UpdatedBy attribute of Tasks entity

--- src/main/java/web/dao/impl/jpa/ReportsDAOImpl.java
+++ src/main/java/web/dao/impl/jpa/ReportsDAOImpl.java
@@ -28,11 +28,11 @@ private EntityManager entityManager;
     	StringBuffer queryString = new StringBuffer();
     			queryString.append( "SELECT task FROM Tasks AS task ");
     			queryString.append( "WHERE task.parentBox.parentBoard.id = :boardId ");
-    			queryString.append(  "AND task.creationDateTime BETWEEN :startDate AND :endDate " );
+    			queryString.append(  "AND task.createdDate BETWEEN :startDate AND :endDate " );
 
     			if(orderBy != null){
     				if(orderBy.equals("date")){
-    						queryString.append("ORDER BY task.creationDateTime");
+    						queryString.append("ORDER BY task.createdDate");
     				}
                     if(orderBy.equals("status")){
                         queryString.append("ORDER BY task.status");
